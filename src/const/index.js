10818ca1fa040c5657d18511e3e5eb967bc1097b
LeoToti <Londra27?>
2021-05-26 15:48:33 +0200

    fixing .env

--- src/const/index.js
+++ src/const/index.js
@@ -1,1 +1,1 @@
-export const backend_url = process.env.NODE_ENV==='production'?process.env.REACT_APP_BACKEND_URL:REACT_APP_BACKEND_DEV_URL
+export const backend_url = process.env.NODE_ENV==='production'?process.env.REACT_APP_BACKEND_URL:process.env.REACT_APP_BACKEND_DEV_URL
